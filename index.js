b7fe0fd9317f0563ef635ac92ff70c8888306d8e
Vinnycios <vinnyciosdev@gmail.com>
2023-11-23 02:05:18 -0300

    .

--- index.js
+++ index.js
@@ -3,28 +3,33 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = new MongoClient(process.env.MONGO_URI);
-
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log("index working");
+// Middleware para conectar ao banco de dados antes de processar as rotas
+app.use(async (req, res, next) => {
+  try {
+    const client = new MongoClient(process.env.MONGO_URI);
+    await client.connect();
+    req.dbClient = client; // Adiciona o cliente do banco de dados ao objeto de requisição
+    next();
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error);
+    res.status(500).send("Erro interno no servidor");
+  }
 });
-
-
-
-await client.connect();
-const db = client.db("enem_questions").collection("questions");
-
-const questions = await db.aggregate([{ $sample: { size: 10 } }]).toArray();
-
-console.log(questions);
-
 
 app.get("/test", async (request, response) => {
-  response.send(questions);
+  try {
+    const db = request.dbClient.db("enem_questions").collection("questions");
+    const questions = await db.aggregate([{ $sample: { size: 10 } }]).toArray();
+    response.send(questions);
+  } catch (error) {
+    console.error("Erro ao processar a rota /test:", error);
+    response.status(500).send("Erro interno no servidor");
+  }
 });
 
-await client.close();
-
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
