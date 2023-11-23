ff5ca0b1c38e04bea42217bb4ee2069f87e60234
Vinnycios <vinnyciosdev@gmail.com>
2023-11-23 01:37:24 -0300

    working the api

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
 const client = new MongoClient(process.env.MONGO_URI);
-async function run() {
-  try {
-    await client.connect();
 
-    const db = client.db("enem_questions");
-    const collection = db.collection("questions");
-    /* const collection = client.db("enem_questions").collection("questions"); */
+const app = express();
+const PORT = 3000;
 
-    const questions = collection.aggregate(
-      [ { $sample: { size: 10 } } ]
-    )
+app.listen(PORT, () => {
+  console.log("index working");
+});
 
-    await questions.forEach(console.log);
 
-  } catch (e) {
-    console.log(e);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
 
-run().catch(console.dir);
+await client.connect();
+const db = client.db("enem_questions").collection("questions");
+
+const questions = await db.aggregate([{ $sample: { size: 10 } }]).toArray();
+
+console.log(questions);
+
+
+app.get("/test", async (request, response) => {
+  response.send(questions);
+});
+
