a31d3f86d7ba46fcd5c61ae536cdce17386bdefe
Vinnycios <vinnyciosdev@gmail.com>
2023-11-23 10:15:33 -0300

    2

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import express from "express";
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
-const client = new MongoClient(process.env.MONGO_URI);
-await client.connect()
 
 const PORT = 3000;
 
